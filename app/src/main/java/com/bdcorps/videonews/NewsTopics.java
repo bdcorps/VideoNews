bf673bdcd27192ed4ace132c4fe7b038cb7d6f75
abhinav <abinavharda33@gmail.com>
2016-07-26 18:18:34 -0400

    topics grid design

--- app/src/main/java/com/bdcorps/videonews/NewsTopics.java
+++ app/src/main/java/com/bdcorps/videonews/NewsTopics.java
@@ -8,48 +8,93 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.GridView;
 
-import java.util.ArrayList;
-
 public class NewsTopics extends AppCompatActivity {
 
     GridView topicsGrid;
     public static String[] topicNames = {
-            "home",
-            "opinion",
-            "world",
-            "national",
-            "politics",
-            "upshot",
-            "nyregion",
-            "business",
-            "technology",
-            "science",
-            "health",
-            "sports",
-            "arts",
-            "books",
-            "movies",
-            "theater",
-            "sundayreview",
-            "fashion",
-            "tmagazine",
-            "food",
-            "travel",
-            "magazine",
-            "realestate",
-            "automobiles",
-            "obituaries",
-            "insider"};
+            "Home",
+            "Opinion",
+            "World",
+            "National",
+            "Politics",
+            "Upshot",
+            "New York Region",
+            "Business",
+            "Technology",
+            "Science",
+            "Health",
+            "Sports",
+            "Arts",
+            "Books",
+            "Movies",
+            "Theater",
+            "Sunday Review",
+            "Fashion",
+            "T Magazine",
+            "Food",
+            "Travel",
+            "Magazine",
+            "Real Estate",
+            "Automobiles",
+            "Obituaries",
+            "Insider"};
     public static int[] topicLogos = {
             R.drawable.ic_home_white_24dp,
-            R.drawable.ic_forum_white_24dp,
-            R.drawable.ic_public_white_24dp,
+            R.drawable.ic_opinion_white_24dp,
+            R.drawable.ic_world_white_24dp,
+            R.drawable.ic_national_white_24dp,
+            R.drawable.ic_politics_white_24dp,
             R.drawable.ic_cast_light,
-            R.drawable.ic_assessment_white_24dp,
+            R.drawable.ic_cast_light,
+            R.drawable.ic_business_white_24dp,
+            R.drawable.ic_technology_white_24dp,
+            R.drawable.ic_cast_light,
+            R.drawable.ic_health_white_24dp,
+            R.drawable.ic_cast_light,
+            R.drawable.ic_arts_white_24dp,
+            R.drawable.ic_books_white_24dp,
+            R.drawable.ic_movies_white_24dp,
+            R.drawable.ic_theater_white_24dp,
+            R.drawable.ic_sundayreview_white_24dp,
             R.drawable.ic_cast_light,
             R.drawable.ic_cast_light,
+            R.drawable.ic_food_white_24dp,
+            R.drawable.ic_travel_white_24dp,
+            R.drawable.ic_magazine_white_24dp,
+            R.drawable.ic_realestate_white_24dp,
+            R.drawable.ic_automobiles_white_24dp,
             R.drawable.ic_cast_light,
+            R.drawable.ic_insider_white_24dp,
             R.drawable.ic_cast_light};
+
+    public static String[] topicColors = {
+            "blue_grey_500",
+            "green_500",
+            "blue_500",
+            "red_500",
+            "amber_500",
+            "teal_500",
+            "grey_800",
+            "green_A400",
+            "light_blue_600",
+            "indigo_500",
+            "red_800",
+            "purple_500",
+            "yellow_500",
+            "brown_500",
+            "red_500",
+            "blue_grey_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "pink_500",
+            "grey_500"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +104,7 @@ public class NewsTopics extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         topicsGrid = (GridView) findViewById(R.id.topics_gridview);
-        topicsGrid.setAdapter(new TopicsAdapter(getBaseContext(), topicNames,topicLogos));
+        topicsGrid.setAdapter(new TopicsAdapter(getBaseContext(), topicNames, topicLogos, topicColors));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/bdcorps/videonews/TopicsAdapter.java
+++ app/src/main/java/com/bdcorps/videonews/TopicsAdapter.java
@@ -1,6 +1,9 @@
 package com.bdcorps.videonews;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,68 +15,72 @@ import android.widget.Toast;
 /**
  * Created by gdesi on 2016-07-26.
  */
-public class TopicsAdapter extends BaseAdapter{
+public class TopicsAdapter extends BaseAdapter {
 
-        String [] topicNames;
-        Context context;
-        int [] logoIDs;
-        private static LayoutInflater inflater=null;
-        public TopicsAdapter(Context context, String[] topicNames, int[] topicLogos) {
-            // TODO Auto-generated constructor stub
-            this.topicNames =topicNames;
-            this.context=context;
-            logoIDs =topicLogos;
-            inflater = (LayoutInflater)context.
-                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    String[] topicNames;
+    Context context;
+    int[] topicLogos;
+    String[] topicColors;
 
-        }
+    private static LayoutInflater inflater = null;
 
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return topicNames.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            // TODO Auto-generated method stub
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            // TODO Auto-generated method stub
-            return position;
-        }
-
-        public class Holder
-        {
-            TextView tv;
-            ImageView img;
-        }
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
-            Holder holder=new Holder();
-            View rowView;
-
-            rowView = inflater.inflate(R.layout.topic_item_frag, null);
-            holder.tv=(TextView) rowView.findViewById(R.id.textView1);
-            holder.img=(ImageView) rowView.findViewById(R.id.imageView1);
-
-            holder.tv.setText(topicNames[position]);
-            holder.img.setImageResource(logoIDs[position]);
-
-            rowView.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    // TODO Auto-generated method stub
-                    Toast.makeText(context, "You Clicked "+ topicNames[position], Toast.LENGTH_LONG).show();
-                }
-            });
-
-            return rowView;
-        }
+    public TopicsAdapter(Context context, String[] topicNames, int[] topicLogos, String[] topicColors) {
+        this.topicNames = topicNames;
+        this.context = context;
+        this.topicLogos = topicLogos;
+        this.topicColors = topicColors;
+        inflater = (LayoutInflater) context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
     }
+
+    @Override
+    public int getCount() {
+        return topicNames.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public class Holder {
+        CardView topicCardView;
+        TextView topicName;
+        ImageView topicLogo;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Holder holder = new Holder();
+        View rowView;
+
+        rowView = inflater.inflate(R.layout.topic_item_frag, null);
+        holder.topicName = (TextView) rowView.findViewById(R.id.topic_name);
+        holder.topicLogo = (ImageView) rowView.findViewById(R.id.topic_logo);
+        holder.topicCardView = (CardView) rowView.findViewById(R.id.topic_cardview);
+
+        holder.topicName.setText(topicNames[position]);
+        holder.topicLogo.setImageResource(topicLogos[position]);
+
+        final int cd_back_color = context.getResources().getIdentifier(topicColors[position], "color", context.getPackageName());
+
+        holder.topicCardView.setCardBackgroundColor(ContextCompat.getColor(context, cd_back_color));
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "You Clicked " + topicNames[position], Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return rowView;
+    }
+
+}
